68dc65d406bb6d4cc41bf3d156081c84fbf86935
ownermood2 <moinuddinraza3123@gmail.com>
2026-02-01 14:19:55 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,13 +1,11 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
-# Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
 API_ID = int(environ.get("API_ID", "29136894"))
 API_HASH = environ.get("API_HASH", "88f3d07b70de48ac1fc13866b0c9e562")
-BOT_TOKEN = environ.get("8520965543:AAHNrK-KpnqZ-3gI4lmmhv9pYeqgWZSraN0", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8520965543:AAFBrr5VP-xaz0iLAe8ktFklBzKFD-RmFFI")
+
 OWNER = int(environ.get("OWNER", "8494053059"))
-CREDIT = environ.get("CREDIT", "𝐂𝐋𝐀𝐓 𝐎𝐖𝐍𝐄𝐑")
-LOG_CHANNEL = "-1002631537674" # log group me bot ko admin narur banaye full rights ke sath
-#WEBHOOK = True  # Don't change this
-#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+CREDIT = environ.get("CREDIT", "CLAT OWNER")
+
+LOG_CHANNEL = "-1003711131531"
