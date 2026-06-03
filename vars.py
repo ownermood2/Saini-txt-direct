a18468da3c5d616ff65c81f596039387da6dc692
ownermood2 <ownermood1@gmail.com>
2026-06-03 12:59:57 +0000

    Update: new BOT_TOKEN

--- vars.py
+++ vars.py
@@ -3,7 +3,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29136894"))
 API_HASH = environ.get("API_HASH", "88f3d07b70de48ac1fc13866b0c9e562")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8520965543:AAFBrr5VP-xaz0iLAe8ktFklBzKFD-RmFFI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8957567289:AAFPt4Cn2ktjzh9lhp68uVn8XiwbQJgSg1Q")
 
 OWNER = int(environ.get("OWNER", "8494053059"))
 CREDIT = environ.get("CREDIT", "CLAT OWNER")
